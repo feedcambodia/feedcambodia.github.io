6ae4e48f2678cedbc7905d28bb924319c72ef156
chris <genxstocks@yahoo.com>
2016-10-17 20:26:54 -0700

    added videos

---
+++ js/controllers/player.js
@@ -0,0 +1,15 @@
+//*****************************************************************************************************************
+// Copyright � 2016 Feed. All rights reserved.
+// You may not copy, reproduce, republish, disassemble, decompile, reverse engineer, post, broadcast, transmit, or
+// make available to the public any content or code on this website without prior written permission from Feed.
+//*****************************************************************************************************************
+
+'use strict';
+
+var ctl = angular.module('PlayerController', ['Resources']);
+
+ctl.controller('PlayerCtrl', function ($scope, $routeParams, $sce, $location, LogResource) {
+  LogResource($location.$$path).Post();
+  var playlistId = $routeParams.playlistId;
+  $scope.videoUrl = $sce.trustAsResourceUrl("https://www.youtube.com/embed?listType=playlist&list=" + playlistId);
+});
---
+++ js/controllers/video.js
@@ -0,0 +1,19 @@
+//*****************************************************************************************************************
+// Copyright � 2016 Feed. All rights reserved.
+// You may not copy, reproduce, republish, disassemble, decompile, reverse engineer, post, broadcast, transmit, or
+// make available to the public any content or code on this website without prior written permission from Feed.
+//*****************************************************************************************************************
+
+'use strict';
+
+var ctl = angular.module('VideoController', ['Resources']);
+
+ctl.controller('VideoCtrl', function ($scope, $routeParams, $location, LogResource, YoutubeResource) {
+  LogResource($location.$$path).Post();
+  $scope.videos = YoutubeResource().Get({
+    'part': 'snippet',
+    'channelId': 'UCO3EB-nz80N1VHyYaG81G3A',
+    'maxResults': 50,
+    'key': 'AIzaSyAhXUfc4ISI65ORq9BAUgrNTehsV6f_oIk'
+  });
+});
