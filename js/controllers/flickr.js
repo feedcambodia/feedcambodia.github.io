c66b1d692fd79ae98e84d5e6ca95824e2d923af4
Chris Davison <genxstocks@live.com>
2017-10-28 10:36:06 -0700

    license info

--- js/controllers/flickr.js
+++ js/controllers/flickr.js
@@ -1,14 +1,14 @@
 //*****************************************************************************************************************
 // Copyright 2014 - 2017 Lahuna. All rights reserved.
 // You may not copy, reproduce, republish, disassemble, decompile, reverse engineer, post, broadcast, transmit, or
-// make available to the public any content or code on this website without prior written permission from Chanthu Oeur.
+// make available to the public any content or code on this website without prior written permission from Lahuna.
 //*****************************************************************************************************************
 
 'use strict';
 
 var ctl = angular.module('FlickrController', ['Resources']);
 
-ctl.controller('FlickrSearchCtrl', function ($scope, $routeParams, $location,
+ctl.controller('FlickrSearchCtrl', function ($scope, $routeParams, $location, $sce,
   LogResource, FlickrResource, FlickrLicenseResource) {
   LogResource.Post({
     'path': $location.$$path
@@ -83,6 +83,7 @@ ctl.controller('FlickrSearchCtrl', function ($scope, $routeParams, $location,
     for (var x in lics) {
       if (lics[x].id == $scope.license) {
         $scope.licText = lics[x].name;
+        $scope.licUrl = $sce.trustAsUrl(lics[x].url);
         break;
       }
     }
